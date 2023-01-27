48898b9739eedf715f58bbb562509ac916a71b08
ESHFFD <ehsanshamsfallah@gmail.com>
2023-01-27 11:52:44 +0330

    onBoardingViewModel veiw is developing...

--- lib/presentation/base/base_model_view.dart
+++ lib/presentation/base/base_model_view.dart
@@ -1,8 +1,9 @@
-abstract class BaseViewModel {}
+abstract class BaseViewModel extends BaseViewModelInput
+    with BaseViewModelOutput {}
 
 abstract class BaseViewModelInput {
-  void start();
-  void dispose();
+  void start(); // called in init state
+  void dispose(); // calle when the code died
 }
 
 abstract class BaseViewModelOutput {}
---
+++ lib/presentation/onBoarding/onboarding_view_mdoel.dart
@@ -0,0 +1,37 @@
+import 'package:adv_app/presentation/base/base_model_view.dart';
+
+class OnBoardingViewModel extends BaseViewModel
+    with OnBoardingViewModelInput, OnBoardingViewModelOutput {
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+
+  @override
+  void start() {
+    // TODO: implement start
+  }
+
+  @override
+  void goNext() {
+    // TODO: implement goNext
+  }
+
+  @override
+  void goPrevious() {
+    // TODO: implement goPrevious
+  }
+
+  @override
+  void onPageChanged(int index) {
+    // TODO: implement onPageChanged
+  }
+}
+
+abstract class OnBoardingViewModelInput {
+  void goNext();
+  void goPrevious();
+  void onPageChanged(int index);
+}
+
+abstract class OnBoardingViewModelOutput {}
