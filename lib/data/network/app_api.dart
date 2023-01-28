17599d707b56729b684d4b2197557abd1beb09ab
ESHFFD <ehsanshamsfallah@gmail.com>
2023-01-28 13:56:24 +0330

    adding customers and auth and contacts class in domain folder

--- lib/data/network/app_api.dart
+++ lib/data/network/app_api.dart
@@ -1,8 +1,13 @@
 import 'package:adv_app/app/constant.dart';
+import 'package:adv_app/data/responses/responses.dart';
 import 'package:dio/dio.dart';
 import 'package:retrofit/retrofit.dart';
+part 'app_api.g.dart';
 
 @RestApi(baseUrl: Constant.baseUrl)
 abstract class AppServiceClient {
   factory AppServiceClient(Dio dio, {String baseUrl}) = _AppServiceClient;
+
+  @POST('/customers/login')
+  Future<AuthenticationResponses> login();
 }
--- lib/domain/model.dart
+++ lib/domain/model.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 class SliderObject {
   final String title;
   final String subtitle;
@@ -6,5 +7,36 @@ class SliderObject {
     required this.title,
     required this.subtitle,
     required this.image,
+  });
+}
+
+class Customers {
+  String id;
+  String name;
+  int numOfNotification;
+  Customers({
+    required this.id,
+    required this.name,
+    required this.numOfNotification,
+  });
+}
+
+class Contacts {
+  String phone;
+  String email;
+  String link;
+  Contacts({
+    required this.phone,
+    required this.email,
+    required this.link,
+  });
+}
+
+class Authentication {
+  Customers customers;
+  Contacts contacts;
+  Authentication({
+    required this.customers,
+    required this.contacts,
   });
 }
